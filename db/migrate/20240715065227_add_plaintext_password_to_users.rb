9753832db023feed7c6c21c5153d0a5a33a26734
ManikantaAyinam123 <manikanta@actimize.in>
2024-07-18 15:21:51 +0530

    pass

--- db/migrate/20240715065227_add_plaintext_password_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPlaintextPasswordToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :plaintext_password, :string
-  end
-end
--- db/migrate/20240715065948_add_party_to_voters.rb
+++
@@ -1,5 +0,0 @@
-class AddPartyToVoters < ActiveRecord::Migration[7.1]
-  def change
-    add_column :voters, :party, :string
-  end
-end
